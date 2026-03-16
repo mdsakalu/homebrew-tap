bf589da35ff3f417094b637933adb764d21795c6
Michael Sakaluk <mdsakalu@users.noreply.github.com>
2026-03-16 12:23:20 -0400

    Update herald formula to use prebuilt app bundle

--- Formula/herald.rb
+++ Formula/herald.rb
@@ -1,32 +1,15 @@
 class Herald < Formula
   desc "Modern macOS notification CLI built on UNUserNotificationCenter"
   homepage "https://github.com/mdsakalu/herald"
-  url "https://github.com/mdsakalu/herald/archive/refs/tags/v#{version}.tar.gz"
+  url "https://github.com/mdsakalu/herald/releases/download/v0.1.0/herald-v0.1.0.tar.gz"
   license "MIT"
   version "0.1.0"
 
   depends_on :macos
-  depends_on xcode: ["15.0", :build]
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-
-    # Create app bundle
-    app_dir = libexec/"Herald.app/Contents"
-    (app_dir/"MacOS").mkpath
-    (app_dir/"Resources").mkpath
-
-    cp buildpath/".build/release/Herald", app_dir/"MacOS/herald"
-
-    # Stamp version into Info.plist
-    inreplace "resources/Info.plist", "__VERSION__", version.to_s
-    cp "resources/Info.plist", app_dir/"Info.plist"
-
-    # Ad-hoc sign
-    system "codesign", "--force", "--deep", "--sign", "-", libexec/"Herald.app"
-
-    # Symlink binary into bin
-    (bin/"herald").make_symlink(app_dir/"MacOS/herald")
+    prefix.install "Herald.app"
+    (bin/"herald").make_symlink(prefix/"Herald.app/Contents/MacOS/herald")
   end
 
   test do
