ea58244c3ddd2e8aec8828c4406f0607ff5eef83
Michael Sakaluk <mdsakalu@users.noreply.github.com>
2026-03-20 17:48:25 -0400

    fix: update herald formula sha256 for v0.1.0

--- Formula/herald.rb
+++ Formula/herald.rb
@@ -2,7 +2,7 @@ class Herald < Formula
   desc "Modern macOS notification CLI built on UNUserNotificationCenter"
   homepage "https://github.com/mdsakalu/herald"
   url "https://github.com/mdsakalu/herald/releases/download/v0.1.0/herald-v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "516a3cce57aad39fb6795c3a00606b93d7804f71e39b2a74ce76b8019b64d120"
   license "MIT"
   version "0.1.0"
 
