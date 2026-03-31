930bf879185173ad66a8ddca60d19c55ac1ec6b1
Michael Sakaluk <mdsakalu@users.noreply.github.com>
2026-03-31 11:19:17 -0400

    herald 0.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/herald.rb
+++ Formula/herald.rb
@@ -1,10 +1,10 @@
 class Herald < Formula
   desc "Modern macOS notification CLI built on UNUserNotificationCenter"
   homepage "https://github.com/mdsakalu/herald"
-  url "https://github.com/mdsakalu/herald/releases/download/v0.1.0/herald-v0.1.0.tar.gz"
-  sha256 "516a3cce57aad39fb6795c3a00606b93d7804f71e39b2a74ce76b8019b64d120"
+  url "https://github.com/mdsakalu/herald/releases/download/v0.2.0/herald-v0.2.0.tar.gz"
+  sha256 "d09bef12690a7a75ba2bd1aa793b574b964f2fdcecbe44b7309dcedde2e59b01"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   depends_on :macos
 
